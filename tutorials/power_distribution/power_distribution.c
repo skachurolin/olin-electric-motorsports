d435cff53dfa5bcfa4da9f4f150dd47699562fa9
ayushchakra <achakraborty@olin.edu>
2022-07-22 08:23:54 -0400

    revised power distribution tutorial

--- tutorials/power_distribution/power_distribution.c
+++ tutorials/power_distribution/power_distribution.c
@@ -5,21 +5,21 @@ gpio_t DASHBOARD_POWER = PB0;
 gpio_t SERVICE_SECTION_POWER = PB1;
 gpio_t THROTTLE_POWER = PB2;
 
-void power_device(gpio_t pin) {
-    gpio_set_pin(pin);
+void power_device(gpio_t power_device) {
+    gpio_set_pin(power_device);
 }
 
-void power_all() {
+void power_all(void) {
     gpio_set_pin(DASHBOARD_POWER);
     gpio_set_pin(SERVICE_SECTION_POWER);
     gpio_set_pin(THROTTLE_POWER);
 }
 
-void power_off_device(gpio_t pin) {
-    gpio_clear_pin(pin);
+void power_off_device(gpio_t power_device) {
+    gpio_clear_pin(power_device);
 }
 
-void power_off_all() {
+void power_off_all(void) {
     gpio_clear_pin(DASHBOARD_POWER);
     gpio_clear_pin(SERVICE_SECTION_POWER);
     gpio_clear_pin(THROTTLE_POWER);
